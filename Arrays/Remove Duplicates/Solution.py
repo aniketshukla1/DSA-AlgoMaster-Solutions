f6a7b199c611a2b95a35895a4156b935e4443f55
Aniket Shukla <aniketshukla619@gmail.com>
2025-01-04 14:33:46 +0530

    Modified Remove Duplicates Solution

--- Arrays/Remove Duplicates/Solution.py
+++ Arrays/Remove Duplicates/Solution.py
@@ -2,11 +2,9 @@ from typing import List
 
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
-        unique_element_idx = 0
+        unique_idx = 0
         for idx in range(1, len(nums)):
-            if nums[idx] != nums[unique_element_idx]:
-                unique_element_idx += 1
-                nums[idx], nums[unique_element_idx] = nums[unique_element_idx], nums[idx]
-            else:
-                continue
-        return unique_element_idx + 1
+            if nums[unique_idx] != nums[idx]:
+                unique_idx += 1
+                nums[unique_idx] = nums[idx]
+        return unique_idx + 1
